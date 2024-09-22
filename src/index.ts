c4ed8a5514699b6618c35d9b3b4b54a5c1735472
Abdul Nawab <abdulnawab98@gmail.com>
2024-09-21 17:23:28 -0700

    added proper ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { MikroORM } from "@mikro-orm/core"
+import { MikroORM } from "@mikro-orm/postgresql"
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: process.env.NODE_ENV === 'production'
+
+    });   
+}
+
 console.log("hello world")
